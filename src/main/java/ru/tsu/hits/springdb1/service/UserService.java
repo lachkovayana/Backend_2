045f89cb2d7b81e2a32404b9d8ed4b7b43741a83
Yana Lachkova <lyana688@gmail.com>
2022-04-04 01:59:34 +0700

    sha-256 for password added

--- src/main/java/ru/tsu/hits/springdb1/service/UserService.java
+++ src/main/java/ru/tsu/hits/springdb1/service/UserService.java
@@ -12,26 +12,49 @@ import ru.tsu.hits.springdb1.dto.UserDto;
 import ru.tsu.hits.springdb1.entity.UserEntity;
 import ru.tsu.hits.springdb1.repository.UserRepository;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
+    public static byte[] digest(byte[] input, String algorithm) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(e);
+        }
+        byte[] result = md.digest(input);
+        return result;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
     @Transactional
     public UserDto save(CreateUpdateUserDto createUpdateUserDto) {
+        byte[] shaInBytes = digest(createUpdateUserDto.getPassword().getBytes(UTF_8), "SHA-256");
+
         var entity = new UserEntity(
                 UUID.randomUUID().toString(),
                 createUpdateUserDto.getCreationDate(),
                 createUpdateUserDto.getEditDate(),
                 createUpdateUserDto.getFullName(),
                 createUpdateUserDto.getEmail(),
-                createUpdateUserDto.getPassword(),
+                bytesToHex(shaInBytes),
                 createUpdateUserDto.getRole()
         );
         var savedEntity = userRepository.save(entity);
